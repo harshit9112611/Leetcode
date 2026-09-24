1321b220b4a08a25c8410cf75d22d99835372100
Harshit Verma <2507harshit@gmail.com>
2026-09-25 00:53:32 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (12.56%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,34 +1,44 @@
 class Solution {
-    public boolean search(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
+    public boolean search(int[] nums, int k) {
+        boolean ans=false;
+      int pivot= findPivot(nums);
+      if(pivot==-1) {
+        return binarySearch(nums,k,0,nums.length-1);
+      }
+      else {
+        boolean firstHalf=binarySearch(nums,k,0,pivot);
+        ans=firstHalf;
+        if(!firstHalf) {
+            return binarySearch(nums,k,pivot+1,nums.length-1);
+        }
+        return ans;
+      }
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+    }
 
-            if (nums[mid] == target) {
-                return true;
-            }
-            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
-                low++;
-                high--;
-                continue;
-            }
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            } 
-            else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
+    public int findPivot(int[] nums) {
+        int pivot=-1;
+        for(int i=0;i<nums.length-1;i++) {
+            if(nums[i+1]<nums[i]) {
+                pivot=i;
             }
         }
+        return pivot;
+    }
 
+    public boolean binarySearch(int[] nums,int k,int low,int high) {
+        while(low<=high) {
+            int mid=low+(high-low)/2;
+            if(k==nums[mid]) {
+                return true;
+            }
+            else if(k<nums[mid]) {
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
+            }
+        }
         return false;
     }
 }
